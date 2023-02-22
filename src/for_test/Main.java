603e77a9e4c817ad8ade1ee6dff12eac1b06100d
denchik <denis13071981@mail.ru>
2023-02-22 15:02:01 +0300

    add test

--- src/for_test/Main.java
+++ src/for_test/Main.java
@@ -3,81 +3,26 @@ package for_test;
 
 public class Main {
     public static void main(String[] args) {
+        ClearConsole();
 
-//
-//        ArrayList<Integer> list = new ArrayList<>();
-//        Random rand = new Random();
-//        for (int i = 0; i < 10; i++) {
-//            list.add(rand.nextInt(50, 100));
-//        }
-//        Collections.sort(list);
-//        for (int item : list) {
-//            System.out.println(item);
-//        }
+    }
 
-//        ArrayList<String> planets = new ArrayList<>(Arrays.asList("Mars",
-//                "Uranus",
-//                "Earth",
-//                "Saturn",
-//                "Mars",
-//                "Uranus",
-//                "Earth",
-//                "Saturn",
-//                "Pluton",
-//                "Pluton",
-//                "Pluton",
-//                "Jupiter"));
-//
-////        Map<String,Integer> counPlanet = new HashMap<>();
-//
-//        Collections.sort(planets);
-//        int count = 1;
-//
-//        for (int i = 1; i < planets.size(); i++) {
-//            if (planets.get(i - 1).equals(planets.get(i))) {
-//                count++;
-//            } else {
-//                System.out.println(planets.get(i - 1) + " " + count);
-//                count = 1;
-//            }
-//        }
-//        for (int i = 1; i < planets.size(); i++) {
-//            if (planets.get(i-1).equals(planets.get(i))) {
-//                planets.remove(i);
-//                i--;
-//            }
-//        }
-//
-//        Iterator<String> iter = planets.iterator();
-//
-//        while (iter.hasNext()){
-//            iter.next();
-//            iter.remove();
-//        }
-//
-//        System.out.println(planets);
+    public static void ClearConsole() {
+        try {
+            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
 
+            if (operatingSystem.contains("Windows")) {
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
 
-//        List<String> list = new ArrayList<>(Arrays.asList("qwerty",
-//                "asdfg",
-//                "123",
-//                "432",
-//                "456",
-//                "zxcv",
-//                "hytg"));
-//
-//        String tmp;
-//        Iterator<String> iter = list.iterator();
-//        while (iter.hasNext()) {
-//            try {
-//                tmp = iter.next();
-//                Integer.parseInt(tmp);
-//                System.out.println(tmp + " removing");
-//                iter.remove();
-//            }catch (Exception e){
-//                System.out.printf("tmp stay");
-//            }
-//        }
-//        System.out.println(list);
+                startProcess.waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
