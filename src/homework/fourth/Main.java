aa7efc835366ac60317a20d4c928d32a4a541549
denchik <denis13071981@mail.ru>
2023-02-22 12:17:35 +0300

    add fourth homework

--- src/homework/fourth/Main.java
+++ src/homework/fourth/Main.java
@@ -5,14 +5,24 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.console();
         PhoneBook phones = new PhoneBook();
         phones.addContact("Denis");
-        phones.addPhone("Denis", "375291256951");
-        phones.addPhone("Alex", "375291246201");
-        phones.addPhone("Alex", "375291246201");
-//        phones.deletePhone("Denis", "375291256951");
-        phones.addPhone("Alex", "375292236201");
+        phones.addContact("Max", "+375(29)134-56-58");
         phones.showAll();
+        phones.addPhone("Denis", "+375(29)125-69-51");
+        phones.addPhone("Alex", "+375(29)124-62-01");
+        phones.addPhone("Alex", "+375(29)795-52-46");
+        phones.showAll();
+        phones.movePhoneToContact("Alex", "Denis", "+375(29)795-52-46");
+        phones.replaceNumber("Alex", "+375(29)124-62-01", "+375(29)767-34-82");
+        phones.showAll();
+        phones.deleteContact("Max");
+        phones.showAll();
+        phones.find("Alex");
+        phones.find("Max");
+
     }
 }
 
@@ -25,7 +35,17 @@ class PhoneBook {
             HashSet<String> phones = new HashSet<>();
             phoneBook.put(login, phones);
         } else {
-            System.out.println("Contact is there");
+            System.out.printf("Contact \"%s\"is there\n", login);
+        }
+    }
+
+    public void addContact(String login, String phone) {
+        if (!phoneBook.containsKey(login)) {
+            HashSet<String> phones = new HashSet<>();
+            phones.add(phone);
+            phoneBook.put(login, phones);
+        } else {
+            System.out.printf("Contact \"%s\"is there\n", login);
         }
     }
 
@@ -36,42 +56,78 @@ class PhoneBook {
                 phones.add(phone);
                 phoneBook.put(login, phones);
             } else if (phoneBook.get(login).contains(phone)) {
-                System.out.println("This number is there");
+                System.out.println("This number is there\n");
             } else {
                 phoneBook.get(login).add(phone);
             }
         } else {
-            System.out.println("Something wrong");
+            System.out.println("Something wrong\n");
         }
     }
 
-//    public void find(String login) {
-//        ArrayList<String> found = new ArrayList<>();
-//        if (phoneBook.containsKey(login)) {
-//            System.out.println(login);
-//            phoneBook.get(login).forEach(System.out::println);
-//        } else {
-//            System.out.printf("Can`t find user \"%s\"\n", login);
-//        }
-//    }
+    public void movePhoneToContact(String fromContact, String toContact, String phone) {
+        if (phoneBook.containsKey(fromContact) && phoneBook.containsKey(toContact)) {
+            if (phoneBook.get(fromContact).contains(phone)) {
+                addPhone(toContact, phone);
+                deletePhone(fromContact, phone);
+            } else {
+                System.out.println("Something wrong\n");
+            }
+        }
+    }
+
+    public void find(String login) {
+        if (phoneBook.containsKey(login)) {
+            System.out.println("Found contact ↓");
+            System.out.println(login);
+            phoneBook.get(login).forEach(System.out::println);
+            System.out.println("Found contact ↑\n");
+        } else {
+            System.out.printf("Can`t find user \"%s\"\n", login);
+        }
+    }
 
     public void showAll() {
+        System.out.println("All contacts ↓");
         phoneBook.forEach((name, phones) -> {
             System.out.println(name);
             phones.forEach(System.out::println);
+            System.out.println();
         });
+        System.out.println("All contacts ↑\n");
     }
 
-//    public void deleteContact(String name) {
-//        if (phoneBook.containsKey(name)) {
-//            phoneBook.remove(name);
-//        } else {
-//            System.out.println("Not found contact for delete");
-//        }
-//    }
+    public void deleteContact(String name) {
+        if (phoneBook.containsKey(name)) {
+            phoneBook.remove(name);
+        } else {
+            System.out.println("Not found contact for delete");
+        }
+    }
 
-//    public void deletePhone(String name, String phone) {
-//        phoneBook.get(name).remove(phone);
-//    }
+    public void deletePhone(String name, String phone) {
+        if (phoneBook.containsKey(name)) {
+            if (phoneBook.get(name).contains(phone)) {
+                phoneBook.get(name).remove(phone);
+            } else {
+                System.out.println("Not found this number");
+            }
+        } else {
+            System.out.println("User not found");
+        }
+    }
+
+    public void replaceNumber(String name, String oldPhone, String newPhone) {
+        if (phoneBook.containsKey(name)) {
+            if (phoneBook.get(name).contains(oldPhone)) {
+                phoneBook.get(name).remove(oldPhone);
+                phoneBook.get(name).add(newPhone);
+            } else {
+                System.out.println("Not found this number");
+            }
+        } else {
+            System.out.println("User not found");
+        }
+    }
 
 }
