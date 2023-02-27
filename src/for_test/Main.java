56d8f63304f0d5f683a41880e47c8946dc192149
denchik <denis13071981@mail.ru>
2023-02-27 12:37:54 +0300

    add test

--- src/for_test/Main.java
+++ src/for_test/Main.java
@@ -1,28 +1,57 @@
 package for_test;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ClearConsole();
 
-    }
+        int[] nums1 = {1, 2, 3, 2, 1};
+        int[] nums2 = {3, 2, 1, 4, 7};
 
-    public static void ClearConsole() {
-        try {
-            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+        int[] nums3 = {0, 0, 0, 0, 0};
+        int[] nums4 = {0, 0, 0, 0, 0};
 
-            if (operatingSystem.contains("Windows")) {
-                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
-                Process startProcess = pb.inheritIO().start();
-                startProcess.waitFor();
-            } else {
-                ProcessBuilder pb = new ProcessBuilder("clear");
-                Process startProcess = pb.inheritIO().start();
+        System.out.println(Array.findEqualsSubarray(nums1,nums2));
+        System.out.println(Array.findEqualsSubarray(nums3,nums4));
 
-                startProcess.waitFor();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
+
+class Array {
+
+    public static String findEqualsSubarray(int[] arr1, int[] arr2) {
+        List<Integer> result = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i <= arr1.length; i++) {
+            for (int j = i; j <= arr1.length; j++) {
+                int[] tmp = Arrays.copyOfRange(arr1, i, j);
+                if (isContains(tmp, arr2) && count < tmp.length) {
+                    result.clear();
+                    for (int item : tmp) {
+                        result.add(item);
+                    }
+                    count = tmp.length;
+                }
+            }
+        }
+        return "Повторяющийся подмассив с максимальной длиной " + result + " и содержит " + count + " элементов" ;
+    }
+
+    public static boolean isContains(int[] arr, int[] arr2) {
+        for (int i = 0; i <= arr2.length; i++) {
+            for (int j = i; j <= arr2.length; j++) {
+                int[] tmp = Arrays.copyOfRange(arr2, i, j);
+                if (Arrays.equals(arr, tmp)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
