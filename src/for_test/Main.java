c2d1c2510dce65ee6bf2eb12cd4586aa913f5168
denchik <denis13071981@mail.ru>
2023-02-27 12:52:01 +0300

    add test

--- src/for_test/Main.java
+++ src/for_test/Main.java
@@ -8,14 +8,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums1 = {1, 2, 3, 2, 1};
+        int[] nums1 = {1, 3, 2, 1, 7};
         int[] nums2 = {3, 2, 1, 4, 7};
 
         int[] nums3 = {0, 0, 0, 0, 0};
         int[] nums4 = {0, 0, 0, 0, 0};
 
-        System.out.println(Array.findEqualsSubarray(nums1,nums2));
-        System.out.println(Array.findEqualsSubarray(nums3,nums4));
+        int[] nums5 = {1, 2, 4, 3, 2};
+        int[] nums6 = {2, 3, 1, 3, 5};
+
+        System.out.println(Array.findEqualsSubarray(nums1, nums2));
+        System.out.println(Array.findEqualsSubarray(nums3, nums4));
+        System.out.println(Array.findEqualsSubarray(nums5, nums6));
 
     }
 }
@@ -29,7 +33,7 @@ class Array {
         for (int i = 0; i <= arr1.length; i++) {
             for (int j = i; j <= arr1.length; j++) {
                 int[] tmp = Arrays.copyOfRange(arr1, i, j);
-                if (isContains(tmp, arr2) && count < tmp.length) {
+                if (tmp.length > 1 && count < tmp.length && isContains(tmp, arr2)) {
                     result.clear();
                     for (int item : tmp) {
                         result.add(item);
@@ -38,7 +42,11 @@ class Array {
                 }
             }
         }
-        return "Повторяющийся подмассив с максимальной длиной " + result + " и содержит " + count + " элементов" ;
+        if (result.size() < 2) {
+            return "Чёт ничего не нашлось :( ";
+        } else {
+            return "Повторяющийся подмассив с максимальной длиной " + result + " и содержит " + count + " элементов";
+        }
     }
 
     public static boolean isContains(int[] arr, int[] arr2) {
