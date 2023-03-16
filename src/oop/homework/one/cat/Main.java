158dca96a94dd897743252886981278f8c642095
Denis <denis13071981@gmail.com>
2023-03-16 15:59:05 +0300

    some homework

--- src/oop/homework/one/cat/Main.java
+++ src/oop/homework/one/cat/Main.java
@@ -2,6 +2,11 @@ package oop.homework.one.cat;
 
 public class Main {
     public static void main(String[] args) {
-
+        Cat myCat = new Cat("Мурка");
+        myCat.petTheCat();
+        myCat.coming();
+        myCat.byeCorm(2, 10);
+        myCat.showCormAtHome();
+        myCat.showSatiety();
     }
 }
---
+++ src/oop/homework/one/geo/tests/GeoTreeTest.java
@@ -0,0 +1,38 @@
+package oop.homework.one.geo.tests;
+
+import oop.homework.one.geo.GeoTree;
+import oop.homework.one.geo.Person;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoTreeTest {
+
+    private final Person testNaneOne = new Person("Alex");
+    private final Person getTestNaneTwo = new Person("Masha");
+
+    GeoTree geo = new GeoTree();
+
+    @Test
+    void appendMarried() {
+//        assertEquals();
+    }
+
+    @org.junit.jupiter.api.Test
+    void appendBrotherOrSister() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void appendParent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void appendChildren() {
+
+//        assertEquals(test,);
+    }
+
+    @org.junit.jupiter.api.Test
+    void appendPartner() {
+    }
+}
